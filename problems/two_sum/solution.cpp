03522982bb06a1fa2522deab825e5186de877f96
Rus <81392804+koparion@users.noreply.github.com>
2022-03-11 18:13:57 +0000

    Sync LeetCode submission - Two Sum (cpp)

---
+++ problems/two_sum/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        int length = nums.size();
+        for(int i = 0; i < length; i++)
+        {
+            for(int j = i + 1; j < length; j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        length = 0;
+        return {-1,-1};
+    }
+};
