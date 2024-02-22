f2594199ff3dd847459dd31e1b65e7d5d17dd59d
Rus <81392804+koparion@users.noreply.github.com>
2024-02-22 21:29:35 +0000

    Sync LeetCode submission - Two Sum (python3)

--- solved-problems/problems/two_sum/solution.py
+++ solved-problems/problems/two_sum/solution.py
@@ -1,39 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # hashmap = {}
+        hashmap = {}  # Initialize a hashmap to store numbers and their indices
 
-        # for i, value in enumerate(nums): #adding counter using enumerate
-        #     remaining = target - nums[i]
-
-        #     if remaining in hashmap:
-        #         return[i, hashmap[remaining]]
-        #     else:
-        #         hashmap[value] = i
-         # Iterate through the indices of nums
-
-        ## another way
-        # hashmap = {}
-        # for i in range(len(nums)):
-        #     value = nums[i]
-        #     sum = target - value
-
-        #     # Check if the remaining value exists in hashmap
-        #     if sum in hashmap:
-        #         # If yes, return the indices of the current element and the index of its complement
-        #         return [hashmap[sum], i]
-        #     else:
-        #         # If not, add the current value and its index to hashmap
-        #         hashmap[value] = i
-
-        ## 3rd way
-        seen = set()
-
+        # Iterate through the indices of the input list 'nums'
         for i in range(len(nums)):
-            complement = target - nums[i]
+            difference = target - nums[i]  # Calculate the difference between the target and the current number
+            
+            # Check if the difference exists in the hashmap
+            if difference in hashmap:
+                return [hashmap[difference], i]  # If found, return the indices of the difference and the current number
+            
+            hashmap[nums[i]] = i  # Store the current number and its index in the hashmap
 
-            if complement in seen:
-                # If the complement is in the set, return its index and the current index
-                return [nums.index(complement), i]
-            else:
-                # Otherwise, add the current number and its index to the set
-                seen.add(nums[i])
+        return []  # Return an empty list if no solution is found
